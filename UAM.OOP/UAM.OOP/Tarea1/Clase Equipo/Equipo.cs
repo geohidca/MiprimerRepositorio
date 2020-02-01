90c7ea7f6ab93638376568fa7d2c1911392b7749
Geovanny Hidalgo <geohidca@gmail.com>
2020-02-01 12:44:43 -0600

    Agrego la Tarea1 al programa

---
+++ UAM.OOP/UAM.OOP/Tarea1/Clase Equipo/Equipo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAM.OOP.Tarea1
+{
+    class Equipo
+    {
+        #region Atributos
+        private int _ID;
+        private string _nombre;
+        #endregion;
+
+        #region Propiedad
+        public int ID { get => _ID; set => _ID = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        #endregion
+
+
+
+
+    }
+}
---
+++ UAM.OOP/UAM.OOP/Tarea1/ProgramTarea1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAM.OOP.Tarea1
+{
+	public class ProgramTarea1
+	{
+		public static void Main(string[] args)
+		{
+			
+			Console.ReadKey();
+
+           
+		}
+	}
+}
